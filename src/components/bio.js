f81c17299a61fc877be555ddd423f0ed8c229aff
fayepal <fayetandog@gmail.com>
2021-02-17 21:05:57 +0100

    Update home page

--- src/components/bio.js
+++ src/components/bio.js
@@ -17,13 +17,15 @@ const Bio = () => {
       <p style={{
         marginBottom: rhythm(2.5),
       }}>
-         <p>Hi! I'm Faye. A Filipino community manager based in Munich, Germany. I love being involved with tech communities so much that I decided to help others build their own.</p>
+        <h1>Hi! I'm Faye 👋</h1>
+           
+        <p>A community manager based in Munich, Germany 🥨🍺. I love being involved with tech communities so much that I decided to help others build their own.</p>
          
-         <p>I currently work as the Checkmk Community Manager at tribe29. A significant amount of my experience in tech community building came from volunteering with Mozilla, where I helped build local and student communities in my free time.</p>
+         <p>I currently work as the Checkmk Community Manager at tribe29. A significant amount of my experience in tech community building came from volunteering with Mozilla 🦊, where I helped build local and student communities in my free time.</p>
 
          <p>Would be happy to answer questions about community management especially in open source, and how to properly cook rice. DMs open!</p>
       
-         <p>Opinions are my own, please don't use it to generalize my culture, my employer, and all the organizations I am associated with.</p>
+         <p>Opinions are my own.</p>
 
         <p>You may also read my <a href="/blog">blog</a>.</p>
 
